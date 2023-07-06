a95f91c8760a4225f5c25d4a0d980c79eb5fc1d1
Walnut IT <daniel.krentzlin@walnut-it.com>
2023-07-06 09:34:53 +0200

    refactored ignore: long-parameter-list in order to remove it completely.

--- lib/src/templates/default_text_styles.dart
+++ lib/src/templates/default_text_styles.dart
@@ -11,138 +11,82 @@ import 'package:flutter/painting.dart';
 /// The default text color.
 const defaultTextColor = Color(0xFF000000);
 
-/// Returns a text style for heading level 1 (h1).
+/// The default font weight.
+const defaultFontWeight = FontWeight.normal;
+
+/// The default font style.
+const defaultFontStyle = FontStyle.normal;
+
+/// The default letter spacing.
 ///
-/// The [color] parameter specifies the text color.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle h1TextStyle({
-  Color color = defaultTextColor,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+/// Only for smallTextStyle()
+const defaultLetterSpacingSmallTextStyle = -0.2;
+
+/// Returns a text style for heading level 1 (h1).
+TextStyle h1TextStyle() => const TextStyle(
       fontSize: 44.0,
-      color: color,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontWeight: defaultFontWeight,
       height: 1.667,
     );
 
 /// Returns a text style for heading level 2 (h2).
-///
-/// The [color] parameter specifies the text color.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle h2TextStyle({
-  Color color = defaultTextColor,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+TextStyle h2TextStyle() => const TextStyle(
       fontSize: 36.0,
-      color: color,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontWeight: defaultFontWeight,
       height: 1.417,
     );
 
 /// Returns a text style for heading level 3 (h3).
-///
-/// The [color] parameter specifies the text color.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle h3TextStyle({
-  Color color = defaultTextColor,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+TextStyle h3TextStyle() => const TextStyle(
       fontSize: 28.0,
-      color: color,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontWeight: defaultFontWeight,
       height: 1.417,
     );
 
 /// Returns a text style for heading level 4 (h4).
-///
-/// The [color] parameter specifies the text color.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle h4TextStyle({
-  Color color = defaultTextColor,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+TextStyle h4TextStyle() => const TextStyle(
       fontSize: 22.0,
-      color: color,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontWeight: defaultFontWeight,
       height: 1.273,
     );
 
 /// Returns a text style for body text.
-///
-/// The [color] parameter specifies the text color.
-/// The [fontWeight] parameter specifies the font weight.
-/// The [fontStyle] parameter specifies the font style.
-TextStyle bodyTextStyle({
-  Color color = defaultTextColor,
-  FontWeight fontWeight = FontWeight.normal,
-  FontStyle fontStyle = FontStyle.normal,
-}) =>
-    TextStyle(
+TextStyle bodyTextStyle() => const TextStyle(
       fontSize: 18.0,
-      color: color,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontWeight: defaultFontWeight,
       height: 1.5,
-      fontStyle: fontStyle,
+      fontStyle: defaultFontStyle,
     );
 
 /// Returns a small text style.
-///
-/// The [color] parameter specifies the text color.
-/// The [fontStyle] parameter specifies the font style.
-/// The [fontWeight] parameter specifies the font weight.
-/// The [letterSpacing] parameter specifies the letter spacing.
-// ignore: long-parameter-list
-TextStyle smallTextStyle({
-  Color color = defaultTextColor,
-  FontStyle fontStyle = FontStyle.normal,
-  FontWeight fontWeight = FontWeight.normal,
-  double letterSpacing = -0.2,
-}) =>
-    TextStyle(
+TextStyle smallTextStyle() => const TextStyle(
       fontSize: 15.0,
-      color: color,
-      fontStyle: fontStyle,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontStyle: defaultFontStyle,
+      fontWeight: defaultFontWeight,
       height: 1.5,
-      letterSpacing: letterSpacing,
+      letterSpacing: defaultLetterSpacingSmallTextStyle,
     );
 
 /// Returns a very small text style.
-///
-/// The [color] parameter specifies the text color.
-/// The [fontStyle] parameter specifies the font style.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle verySmallTextStyle({
-  Color color = defaultTextColor,
-  FontStyle fontStyle = FontStyle.normal,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+TextStyle verySmallTextStyle() => const TextStyle(
       fontSize: 12.0,
-      color: color,
-      fontStyle: fontStyle,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontStyle: defaultFontStyle,
+      fontWeight: defaultFontWeight,
       height: 1.5,
     );
 
 /// Returns an extra small text style.
-///
-/// The [color] parameter specifies the text color.
-/// The [fontStyle] parameter specifies the font style.
-/// The [fontWeight] parameter specifies the font weight.
-TextStyle extraSmallTextStyle({
-  Color color = defaultTextColor,
-  FontStyle fontStyle = FontStyle.normal,
-  FontWeight fontWeight = FontWeight.normal,
-}) =>
-    TextStyle(
+TextStyle extraSmallTextStyle() => const TextStyle(
       fontSize: 10.0,
-      color: color,
-      fontStyle: fontStyle,
-      fontWeight: fontWeight,
+      color: defaultTextColor,
+      fontStyle: defaultFontStyle,
+      fontWeight: defaultFontWeight,
       height: 1.5,
     );
